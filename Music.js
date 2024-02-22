7005ffb9c30f0bfaecd84d631fbb28d14ac29df1
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-02-22 23:30:43 +0530

    made changes in styling

--- Music.js
+++ Music.js
@@ -3,71 +3,74 @@ let previous = document.getElementById("Previous");
 let next = document.getElementById("Next");
 let audio = document.querySelector("audio");
 let img = document.querySelector("img");
+const container = document.getElementById("container");
 let title = document.getElementById("title");
 let artist = document.getElementById("artist");
 let songs = [
-    {
-        name: "Alone",
-        title: "Alone",
-        artist: "Alan Walker",
-    },
-    {
-        name: "Sugar",
-        title: "sugar & Brownies",
-        artist: "Dharia",
-    },
-    {
-        name: "Peaches",
-        title: "Peaches",
-        artist: "Justin Bieber",
-    },
+  {
+    name: "Alone",
+    title: "Alone",
+    artist: "Alan Walker",
+  },
+  {
+    name: "Sugar",
+    title: "sugar & Brownies",
+    artist: "Dharia",
+  },
+  {
+    name: "Peaches",
+    title: "Peaches",
+    artist: "Justin Bieber",
+  },
 ];
 let isplaying = false;
 let playmusic = () => {
-    isplaying = true;
-    audio.play();
-    play.classList.replace('fa-play', 'fa-pause');
-    img.classList.add("anime");
+  isplaying = true;
+  audio.play();
+  play.classList.replace("fa-play", "fa-pause");
+  img.classList.add("anime");
 };
 
 let pausemusic = () => {
-    isplaying = false;
-    audio.pause();
-    play.classList.replace('fa-pause', 'fa-play');
+  isplaying = false;
+  audio.pause();
+  play.classList.replace("fa-pause", "fa-play");
 };
 
 play.addEventListener("click", () => {
-    if (isplaying == false) {
-        playmusic();
-    } else {
-        pausemusic();
-    };
+  if (isplaying == false) {
+    playmusic();
+  } else {
+    pausemusic();
+  }
 });
 
 const loadsong = (songs) => {
-    title.textContent = songs.title;
-    artist.textContent = songs.artist;
-    audio.src = "Music/" + songs.name + ".mp3";
-    img.src = "images/" + songs.name + ".jpg";
-}
+  title.textContent = songs.title;
+  artist.textContent = songs.artist;
+  audio.src = "Music/" + songs.name + ".mp3";
+  img.src = "images/" + songs.name + ".jpg";
+  container.style.setProperty("--img-src", "url(" + img.src + ")");
+};
+
 songindex = 1;
 const nextsong = () => {
-    songindex = (songindex + 1) % songs.length;
-    loadsong(songs[songindex]);
-    playmusic();
-}
+  songindex = (songindex + 1) % songs.length;
+  loadsong(songs[songindex]);
+  playmusic();
+};
 
 const prevsong = () => {
-    songindex = (songindex - 1 + songs.length) % songs.length;
-    loadsong(songs[songindex]);
-    playmusic();
-}
-next.addEventListener('click', nextsong);
-previous.addEventListener('click', prevsong);
+  songindex = (songindex - 1 + songs.length) % songs.length;
+  loadsong(songs[songindex]);
+  playmusic();
+};
+next.addEventListener("click", nextsong);
+previous.addEventListener("click", prevsong);
 
 //volume control
-let volume_slider = document.querySelector('.volume_slider');
+let volume_slider = document.querySelector(".volume_slider");
 
 function setVolume() {
-    audio.volume = volume_slider.value / 100;
+  audio.volume = volume_slider.value / 100;
 }
